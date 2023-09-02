2f706c2eea6cc5188243137ed00009eddfe7adac
angbur <41025473+angbur@users.noreply.github.com>
2023-09-03 00:39:41 +0200

    test

--- src/slack.ts
+++ src/slack.ts
@@ -65,6 +65,47 @@ async function handleSlashCommand(payload: SlackSlashCommandPayload) {
 				body: JSON.stringify(response),
 			};
 
+			case '/foodfight':
+			const response2 = await slackApi(
+				'views.open',
+				modal({
+					id: 'foodfight-modal',
+					title: 'Start a food fight!',
+					trigger_id: payload.trigger_id,
+					blocks: [
+						blocks.section({
+							text: 'The discourse demands food drama! *Send in your spiciest food takes so we can all argue about them and feel alive.*',
+						}),
+						blocks.input({
+							id: 'opinion',
+							label: 'Deposit your controversial food opinions here.',
+							placeholder:
+								'Example: peanut butter and mayonnaise sandwiches are delicious!',
+							initial_value: payload.text ?? '',
+							hint: 'What do you believe about food that people find appalling? Say it with your chest!',
+						}),
+						blocks.select({
+							id: 'spice_level',
+							label: 'How spicy is this opinion?',
+							placeholder: 'Select a spice level',
+							options: [
+								{ label: 'mild', value: 'mild' },
+								{ label: 'medium', value: 'medium' },
+								{ label: 'spicy', value: 'spicy' },
+								{ label: 'nuclear', value: 'nuclear' },
+							],
+						}),
+					],
+				}),
+			);
+
+			if (!response2.ok) {
+				console.log(response2);
+			}
+
+			break;
+
+
 		default:
 			return {
 				statusCode: 200,
