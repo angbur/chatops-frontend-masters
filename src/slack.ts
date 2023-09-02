31bda966b12af119199cfe6e31506d9ba556e9f7
angbur <41025473+angbur@users.noreply.github.com>
2023-09-02 19:24:43 +0200

    test

--- src/slack.ts
+++ src/slack.ts
@@ -1,60 +1,56 @@
-import type { Handler } from '@netlify/functions';
-
-import { parse } from 'querystring';
-import { verifySlackRequest } from './util/slack';
-
 async function handleSlashCommand(payload: SlackSlashCommandPayload) {
 	switch (payload.command) {
-		case '/hedgehug':
-			return {
-				statusCode: 200,
-				body: 'Witaj w Hedgehug! Aby rozpocząć wybierz opcję z menu poniżej.\n\n' +
-					'*Available Commands:*\n' +
-					'• `/hedgehug` - Display this help message :information_source:\n' +
-					'• `/materialyszkoleniowe` - Link to training materials :book:\n' +
-					'• Add more commands here... :rocket:',
-			};
-		case '/materialyszkoleniowe':
-			return {
-				statusCode: 200,
-				body: 'Materiały szkoleniowe znajdziesz tutaj: https://drive.google.com/file/d/1DVcaqBiqbGn25pcpyAOcBzji2T07ALfv/view?usp=drive_link'
-			};
-
-		default:
-			return {
-				statusCode: 200,
-				body: `Command ${payload.command} is not recognized`,
-			};
-	}
-}
-
-export const handler: Handler = async (event) => {
-	const valid = verifySlackRequest(event);
-
-	/* if (!valid) {
-		console.error('invalid request');
-
+	  case '/hedgehug':
+		// Existing commands
 		return {
-			statusCode: 400,
-			body: 'invalid request',
+		  statusCode: 200,
+		  body: 'Witaj w Hedgehug! Aby rozpocząć wybierz opcję z menu poniżej.\n\n' +
+			'*Available Commands:*\n' +
+			'• `/hedgehug` - Display this help message :information_source:\n' +
+			'• `/materialyszkoleniowe` - Link to training materials :book:\n' +
+			'• Add more commands here... :rocket:',
+		};
+	  case '/materialyszkoleniowe':
+		// Existing command
+		return {
+		  statusCode: 200,
+		  body: 'Materiały szkoleniowe znajdziesz tutaj: https://drive.google.com/file/d/1DVcaqBiqbGn25pcpyAOcBzji2T07ALfv/view?usp=drive_link'
+		};
+	  case '/wolnedyzury':
+		// Generate a list of available shifts and buttons
+		const shifts = [
+		  { date: '2023-09-01', shift: 'Morning Shift' },
+		  { date: '2023-09-02', shift: 'Evening Shift' },
+		  // Add more shifts here...
+		];
+  
+		const attachmentButtons = shifts.map(shift => ({
+		  text: `Assign to ${shift.date}`,
+		  type: 'button',
+		  action_id: `assign_shift_${shift.date}`, // Unique action ID for each button
+		}));
+  
+		const attachment = {
+		  text: 'Available Shifts for September 2023:',
+		  fallback: 'No available shifts.',
+		  callback_id: 'assign_shifts',
+		  color: '#36a64f', // Green color
+		  attachment_type: 'default',
+		  actions: attachmentButtons,
+		};
+  
+		return {
+		  statusCode: 200,
+		  body: JSON.stringify({
+			text: 'Here are the available shifts for September 2023:',
+			attachments: [attachment],
+		  }),
+		};
+	  default:
+		return {
+		  statusCode: 200,
+		  body: `Command ${payload.command} is not recognized`,
 		};
 	}
- */
-
-	const body = parse(event.body ?? '') as SlackPayload;
-
-	if (body.command) {
-		return handleSlashCommand(body as SlackSlashCommandPayload);
-	}
-
-	// TODO handle interactivity (e.g. context commands, modals)
-	if (body.payload) {
-		const payload = JSON.parse(body.payload);
-		return payload;
-	}
-
-	return {
-		statusCode: 200,
-		body: 'TODO: handle Slack commands and interactivity',
-	};
-};
+  }
+  
