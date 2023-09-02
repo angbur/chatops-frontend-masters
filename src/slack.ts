11d980648404bec25217541b0e7eb33656a01d52
angbur <41025473+angbur@users.noreply.github.com>
2023-09-02 22:49:30 +0200

    test

--- src/slack.ts
+++ src/slack.ts
@@ -1,6 +1,6 @@
 import type { Handler } from '@netlify/functions';
 import { parse } from 'querystring';
-import { verifySlackRequest } from './util/slack';
+import { blocks, modal, slackApi, verifySlackRequest } from './util/slack';
 
 async function handleSlashCommand(payload: SlackSlashCommandPayload) {
 	switch (payload.command) {
@@ -28,31 +28,44 @@ async function handleSlashCommand(payload: SlackSlashCommandPayload) {
 				}),
 			};
 		case '/faq':
-			return {
-				statusCode: 200,
-				body: {
-					response_type: 'in_channel',
-					blocks: {
-						trigger_id: payload.trigger_id,
-						view: {
-							type: "section",
-							text: {
-							  text: "*Sally* has requested you set the deadline for the Nano launch project",
-							  type: "mrkdwn"
-							},
-							accessory: {
-							  type: "datepicker",
-							  action_id: "datepicker123",
-							  initial_date: "1990-04-28",
-							  placeholder: {
-								type: "plain_text",
-								text: "Select a date"
-							  }
-							}
-						  }
-					}
-				}
-			};
+			const response = await slackApi(
+				'views.open',
+				modal({
+					id: 'foodfight-modal',
+					title: 'Start a food fight!',
+					trigger_id: payload.trigger_id,
+					blocks: [
+						blocks.section({
+							text: 'The discourse demands food drama! *Send in your spiciest food takes so we can all argue about them and feel alive.*',
+						}),
+						blocks.input({
+							id: 'opinion',
+							label: 'Deposit your controversial food opinions here.',
+							placeholder:
+								'Example: peanut butter and mayonnaise sandwiches are delicious!',
+							initial_value: payload.text ?? '',
+							hint: 'What do you believe about food that people find appalling? Say it with your chest!',
+						}),
+						blocks.select({
+							id: 'spice_level',
+							label: 'How spicy is this opinion?',
+							placeholder: 'Select a spice level',
+							options: [
+								{ label: 'mild', value: 'mild' },
+								{ label: 'medium', value: 'medium' },
+								{ label: 'spicy', value: 'spicy' },
+								{ label: 'nuclear', value: 'nuclear' },
+							],
+						}),
+					],
+				}),
+			);
+
+			if (!response.ok) {
+				console.log(response);
+			}
+
+			break;
 		default:
 			return {
 				statusCode: 200,
