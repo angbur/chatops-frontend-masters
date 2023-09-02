2dc17b057e68b78dda0c83094b233c7bd6f7e2d7
angbur <41025473+angbur@users.noreply.github.com>
2023-09-02 17:06:16 +0200

    update path

--- src/util/salesforce.ts
+++ src/util/salesforce.ts
@@ -16,32 +16,11 @@ export async function salesforceApi() {
 	}).catch((err) => console.error(err));
 
 	if (!res || !res.ok) {
-		console.error(res);
+		return 'Error response from Salesforce API'
 	}
 
 	const data = await res?.json();
+	
 
 	return data;
-}
-
-export async function getNewItems(): Promise<NewItem[]> {
-	const notionData = await salesforceApi();
-
-	const openItems = notionData.results.map((item: NotionItem) => {
-		return {
-			opinion: item.properties.opinion.title[0].text.content,
-			spiceLevel: item.properties.spiceLevel.select.name,
-			status: item.properties.Status.status.name,
-		};
-	});
-
-	return openItems;
-}
-
-export async function saveItem(item: NewItem) {
-	const res = await salesforceApi();
-
-	if (!res.ok) {
-		console.log(res);
-	}
 }
