42b67b8b14889e2cb52c8cfa74937e01a9f4f331
angbur <41025473+angbur@users.noreply.github.com>
2023-09-03 00:57:44 +0200

    test

--- src/slack.ts
+++ src/slack.ts
@@ -1,72 +1,13 @@
 import type { Handler } from '@netlify/functions';
+
 import { parse } from 'querystring';
 import { blocks, modal, slackApi, verifySlackRequest } from './util/slack';
+import { saveItem } from './util/notion';
 
 async function handleSlashCommand(payload: SlackSlashCommandPayload) {
 	switch (payload.command) {
-		case '/hedgehug':
-			return {
-				statusCode: 200,
-				body: 'Witaj w Hedgehug! Aby rozpocząć wybierz opcję z menu poniżej.\n\n' +
-				'*Available Commands:*\n' +
-				'• `/hedgehug` - Display this help message :information_source:\n' +
-				'• `/materialyszkoleniowe` - Link to training materials :book:\n' +
-				'• `/wolnedyzury` - List available shifts :calendar:\n' +
-				'• `/faq` - Frequently Asked Questions :question:\n' +
-				'• Add more commands here... :rocket:',
-			};
-		case '/materialyszkoleniowe':
-			return {
-				statusCode: 200,
-				body: 'Materiały szkoleniowe znajdziesz tutaj: https://drive.google.com/file/d/1DVcaqBiqbGn25pcpyAOcBzji2T07ALfv/view?usp=drive_link'
-			};
-		case '/wolnedyzury':
-			return {
-				statusCode: 200,
-				body: JSON.stringify({
-				text: 'Here are the available shifts for September 2023:',
-				
-				}),
-			};
-		/* case '/faq':
-			return {
-				statusCode: 200,
-				* body: 'Witaj w Hedgehug FAQ! Oto najczęściej zadawane pytania i odpowiedzi na nie:\n\n' +
-				'📚 *Pytanie: Jak Wygląda Interwencja?*\n' +
-				'Odpowiedź: Interwencje polegają na reagowaniu na sytuacje zagrożenia dla zwierząt, takie jak przypadki znęcania się czy wypadki drogowe. Wolontariusze dostają specjalne szkolenie, które pomaga im działać w takich sytuacjach. Interwencje mogą być emocjonalnie trudne, ale nasza wspólna praca przynosi zwierzętom pomoc i nadzieję. :animal_rescue:\n\n' +
-				'🚀 *Pytanie: Jak Się Przygotować do Interwencji?*\n' +
-				'Odpowiedź: Przygotowanie obejmuje zapoznanie się z podstawowymi zasadami interwencji, dostępem do odpowiedniego sprzętu, oraz zdobywaniem doświadczenia podczas dyżurów przy zwierzętach. Nasz zespół zawsze wspiera nowych wolontariuszy i pomaga w przygotowaniu do interwencji. :gear:\n\n' +
-				'🌟 *Pytanie: Co Może Nas Zastać na Miejscu Interwencji?*\n' +
-				'Odpowiedź: Sytuacje interwencyjne mogą być różnorodne i nieprzewidywalne. Możemy napotkać na zwierzęta w stanie krytycznym, konieczność działań ratunkowych, a także sytuacje wymagające interakcji z innymi ludźmi. Nasze doświadczenie i szkolenie pomaga nam efektywnie radzić sobie w różnych sytuacjach. :warning:\n\n' +
-				'🤔 *Pytanie: Czy Wolontariat Jest Trudny?*\n' +
-				'Odpowiedź: Wolontariat w organizacji Hedgehug może być emocjonalnie trudny ze względu na trudne sytuacje, z jakimi stykamy się podczas interwencji. Jednak nasza wspólna praca przynosi ogromną satysfakcję i pomaga zwierzętom, co jest naszą najważniejszą motywacją. :heart:'
-		   
-		  
-			}
-			const response = await slackApi('chat.postMessage', {
-				channel: payload.channel_id,
-				text: 'Witaj w Hedgehug FAQ! Oto najczęściej zadawane pytania i odpowiedzi na nie:',
-				blocks: [
-					blocks.section({
-						text: '📚 *Pytanie: Jak Wygląda Interwencja?*\n' +
-						'Odpowiedź: Interwencje polegają na reagowaniu na sytuacje zagrożenia dla zwierząt, takie jak przypadki znęcania się czy wypadki drogowe. Wolontariusze dostają specjalne szkolenie, które pomaga im działać w takich sytuacjach. Interwencje mogą być emocjonalnie trudne, ale nasza wspólna praca przynosi zwierzętom pomoc i nadzieję. :animal_rescue:'
-					}),
-					blocks.section({
-						text: '🚀 *Pytanie: Jak Się Przygotować do Interwencji?*\n' +
-						'Odpowiedź: Przygotowanie obejmuje zapoznanie się z podstawowymi zasadami interwencji, dostępem do odpowiedniego sprzętu, oraz zdobywaniem doświadczenia podczas dyżurów przy zwierzętach. Nasz zespół zawsze wspiera nowych wolontariuszy i pomaga w przygotowaniu do interwencji. :gear:'
-					}),
-					blocks.section({
-						text: '🌟 *Pytanie: Co Może Nas Zastać na Miejscu Interwencji?*\n'
-					})
-				],
-			});
-			return {
-				statusCode: 200,
-				body: JSON.stringify(response),
-			};
- */
-			case '/faq':
-			const response2 = await slackApi(
+		case '/foodfight':
+			const response = await slackApi(
 				'views.open',
 				modal({
 					id: 'foodfight-modal',
@@ -99,12 +40,11 @@ async function handleSlashCommand(payload: SlackSlashCommandPayload) {
 				}),
 			);
 
-			if (!response2.ok) {
-				console.log(response2);
+			if (!response.ok) {
+				console.log(response);
 			}
 
 			break;
-
 
 		default:
 			return {
@@ -112,16 +52,69 @@ async function handleSlashCommand(payload: SlackSlashCommandPayload) {
 				body: `Command ${payload.command} is not recognized`,
 			};
 	}
+
+	return {
+		statusCode: 200,
+		body: '',
+	};
+}
+
+async function handleInteractivity(payload: SlackModalPayload) {
+	const callback_id = payload.callback_id ?? payload.view.callback_id;
+
+	switch (callback_id) {
+		case '/faq':
+			const data = payload.view.state.values;
+			const fields = {
+				opinion: data.opinion_block.opinion.value,
+				spiceLevel: data.spice_level_block.spice_level.selected_option.value,
+				submitter: payload.user.name,
+			};
+
+			await saveItem(fields);
+
+			await slackApi('chat.postMessage', {
+				channel: 'C0438E823SP',
+				text: `Oh dang, y’all! :eyes: <@${payload.user.id}> just started a food fight with a ${fields.spiceLevel} take:\n\n*${fields.opinion}*\n\n...discuss.`,
+			});
+			break;
+
+		case '/hedgehug':
+			const channel = payload.channel?.id;
+			const user_id = payload.user.id;
+			const thread_ts = payload.message.thread_ts ?? payload.message.ts;
+
+			await slackApi('chat.postMessage', {
+				channel,
+				thread_ts,
+				text: `Hey <@${user_id}>, an opinion like this one deserves a heated public debate. Run the \`/foodfight\` slash command in a main channel to start one!`,
+			});
+
+			break;
+
+		default:
+			console.log(`No handler defined for ${payload.view.callback_id}`);
+			return {
+				statusCode: 400,
+				body: `No handler defined for ${payload.view.callback_id}`,
+			};
+	}
+
+	return {
+		statusCode: 200,
+		body: '',
+	};
 }
 
 export const handler: Handler = async (event) => {
 	const valid = verifySlackRequest(event);
 
 	if (!valid) {
-		console.error('Invalid request');
+		console.error('invalid request');
+
 		return {
 			statusCode: 400,
-			body: 'Invalid request',
+			body: 'invalid request',
 		};
 	}
 
@@ -131,138 +124,14 @@ export const handler: Handler = async (event) => {
 		return handleSlashCommand(body as SlackSlashCommandPayload);
 	}
 
+	// TODO handle interactivity (e.g. context commands, modals)
 	if (body.payload) {
 		const payload = JSON.parse(body.payload);
-		return payload;
-	}	
+		return handleInteractivity(payload);
+	}
 
 	return {
 		statusCode: 200,
 		body: 'TODO: handle Slack commands and interactivity',
 	};
 };
-  
-  const faqBlocks = [
-	{
-		"type": "section",
-		"text": {
-			"type": "plain_text",
-			"text": "MAMY WOLNE TERMINY [WAŻNE]",
-			"emoji": true
-		}
-	},
-	{
-		"type": "section",
-		"text": {
-			"type": "plain_text",
-			"text": "WOLNE TERMINY",
-			"emoji": true
-		}
-	},
-	{
-		"type": "section",
-		"fields": [
-			{
-				"type": "plain_text",
-				"text": "09/09/23",
-				"emoji": true
-			},
-			{
-				"type": "plain_text",
-				"text": "10/09/23",
-				"emoji": true
-			},
-			{
-				"type": "plain_text",
-				"text": "11/09/23",
-				"emoji": true
-			},
-			{
-				"type": "plain_text",
-				"text": "15/09/23",
-				"emoji": true
-			},
-			{
-				"type": "plain_text",
-				"text": "23/09/23",
-				"emoji": true
-			}
-		]
-	},
-	{
-		"type": "input",
-		"element": {
-			"type": "datepicker",
-			"initial_date": "1990-04-28",
-			"placeholder": {
-				"type": "plain_text",
-				"text": "Select a date",
-				"emoji": true
-			},
-			"action_id": "datepicker-action"
-		},
-		"label": {
-			"type": "plain_text",
-			"text": "WYBIERZ TERMIN",
-			"emoji": true
-		}
-	},
-	{
-		"type": "divider"
-	},
-	{
-		"type": "section",
-		"text": {
-			"type": "mrkdwn",
-			"text": "OD 08:00 DO 16:00   DYŻUR KOTY"
-		},
-		"accessory": {
-			"type": "button",
-			"text": {
-				"type": "plain_text",
-				"text": "ZAPISZ MNIE!",
-				"emoji": true
-			},
-			"value": "click_me_123",
-			"action_id": "button-action"
-		}
-	}
-];
-
-const payloadBlocks ={
-    "channel": "T05QGCDEVHU",
-    "attachments": [
-        {
-	        "mrkdwn_in": ["text"],
-            "color": "#36a64f",
-            "pretext": "Optional pre-text that appears above the attachment block",
-            "author_name": "author_name",
-            "author_link": "http://flickr.com/bobby/",
-            "author_icon": "https://placeimg.com/16/16/people",
-            "title": "title",
-            "title_link": "https://api.slack.com/",
-            "text": "Optional `text` that appears within the attachment",
-            "fields": [
-                {
-                    "title": "A field's title",
-                    "value": "This field's value",
-                    "short": false
-                },
-                {
-                    "title": "A short field's title",
-                    "value": "A short field's value",
-                    "short": true
-                },
-                {
-                    "title": "A second short field's title",
-                    "value": "A second short field's value",
-                    "short": true
-                }
-            ],
-            "thumb_url": "http://placekitten.com/g/200/200",
-            "footer": "footer",
-            "footer_icon": "https://platform.slack-edge.com/img/default_application_icon.png",
-            "ts": 123456789
-        }
-    ]
-}
