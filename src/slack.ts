85a5ac61165568442dca80762b33d5d3cf8cf2aa
angbur <41025473+angbur@users.noreply.github.com>
2023-09-02 22:44:23 +0200

    test

--- src/slack.ts
+++ src/slack.ts
@@ -33,21 +33,23 @@ async function handleSlashCommand(payload: SlackSlashCommandPayload) {
 				body: {
 					response_type: 'in_channel',
 					blocks: {
-						"type": "section",
-						"text": {
-						  "text": "*Sally* has requested you set the deadline for the Nano launch project",
-						  "type": "mrkdwn"
-						},
-						"accessory": {
-						  "type": "datepicker",
-						  "action_id": "datepicker123",
-						  "initial_date": "1990-04-28",
-						  "placeholder": {
-							"type": "plain_text",
-							"text": "Select a date"
+						view: {
+							"type": "section",
+							"text": {
+							  "text": "*Sally* has requested you set the deadline for the Nano launch project",
+							  "type": "mrkdwn"
+							},
+							"accessory": {
+							  "type": "datepicker",
+							  "action_id": "datepicker123",
+							  "initial_date": "1990-04-28",
+							  "placeholder": {
+								"type": "plain_text",
+								"text": "Select a date"
+							  }
+							}
 						  }
-						}
-					  }
+					}
 				}
 			};
 		default:
