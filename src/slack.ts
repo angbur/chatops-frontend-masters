de0a0faf193e0dc4c388fd67fa6992a4bb3e419e
angbur <41025473+angbur@users.noreply.github.com>
2023-09-02 12:50:13 +0200

    basic config

--- src/slack.ts
+++ src/slack.ts
@@ -6,45 +6,17 @@ import { saveItem } from './util/notion';
 
 async function handleSlashCommand(payload: SlackSlashCommandPayload) {
 	switch (payload.command) {
-		case '/foodfight':
-			const response = await slackApi(
-				'views.open',
-				modal({
-					id: 'foodfight-modal',
-					title: 'Start a food fight!',
-					trigger_id: payload.trigger_id,
-					blocks: [
-						blocks.section({
-							text: 'The discourse demands food drama! *Send in your spiciest food takes so we can all argue about them and feel alive.*',
-						}),
-						blocks.input({
-							id: 'opinion',
-							label: 'Deposit your controversial food opinions here.',
-							placeholder:
-								'Example: peanut butter and mayonnaise sandwiches are delicious!',
-							initial_value: payload.text ?? '',
-							hint: 'What do you believe about food that people find appalling? Say it with your chest!',
-						}),
-						blocks.select({
-							id: 'spice_level',
-							label: 'How spicy is this opinion?',
-							placeholder: 'Select a spice level',
-							options: [
-								{ label: 'mild', value: 'mild' },
-								{ label: 'medium', value: 'medium' },
-								{ label: 'spicy', value: 'spicy' },
-								{ label: 'nuclear', value: 'nuclear' },
-							],
-						}),
-					],
-				}),
-			);
+		case '/hedgehug':
+			return {
+				statusCode: 200,
+				body: 'Witaj w Hedgehug! Aby rozpocząć wybierz opcję z menu poniżej.',
+			};
 
-			if (!response.ok) {
-				console.log(response);
-			}
-
-			break;
+		case '/materialyszkoleniowe':
+			return {
+				statusCode: 200,
+				body: 'Materiały szkoleniowe znajdziesz tutaj: https://drive.google.com/file/d/1DVcaqBiqbGn25pcpyAOcBzji2T07ALfv/view?usp=drive_link'
+			};
 
 		default:
 			return {
@@ -52,18 +24,13 @@ async function handleSlashCommand(payload: SlackSlashCommandPayload) {
 				body: `Command ${payload.command} is not recognized`,
 			};
 	}
-
-	return {
-		statusCode: 200,
-		body: '',
-	};
 }
 
 async function handleInteractivity(payload: SlackModalPayload) {
 	const callback_id = payload.callback_id ?? payload.view.callback_id;
 
 	switch (callback_id) {
-		case 'foodfight-modal':
+		case 'hedgehug-modal':
 			const data = payload.view.state.values;
 			const fields = {
 				opinion: data.opinion_block.opinion.value,
@@ -87,7 +54,7 @@ async function handleInteractivity(payload: SlackModalPayload) {
 			await slackApi('chat.postMessage', {
 				channel,
 				thread_ts,
-				text: `Hey <@${user_id}>, an opinion like this one deserves a heated public debate. Run the \`/foodfight\` slash command in a main channel to start one!`,
+				text: `Hey <@${user_id}>, an opinion like this one deserves a heated public debate. Run the \`/hedgehug\` slash command in a main channel to start one!`,
 			});
 
 			break;
@@ -99,7 +66,6 @@ async function handleInteractivity(payload: SlackModalPayload) {
 				body: `No handler defined for ${payload.view.callback_id}`,
 			};
 	}
-
 	return {
 		statusCode: 200,
 		body: '',
@@ -109,7 +75,7 @@ async function handleInteractivity(payload: SlackModalPayload) {
 export const handler: Handler = async (event) => {
 	const valid = verifySlackRequest(event);
 
-	if (!valid) {
+	/* if (!valid) {
 		console.error('invalid request');
 
 		return {
@@ -117,7 +83,7 @@ export const handler: Handler = async (event) => {
 			body: 'invalid request',
 		};
 	}
-
+ */
 	const body = parse(event.body ?? '') as SlackPayload;
 
 	if (body.command) {
