811fd0ba3d11bb015b9a3a0d64a81aa753af14b6
angbur <41025473+angbur@users.noreply.github.com>
2023-09-02 23:05:16 +0200

    test

--- src/slack.ts
+++ src/slack.ts
@@ -38,25 +38,6 @@ async function handleSlashCommand(payload: SlackSlashCommandPayload) {
 						blocks.section({
 							text: 'The discourse demands food drama! *Send in your spiciest food takes so we can all argue about them and feel alive.*',
 						}),
-						blocks.input({
-							id: 'opinion',
-							label: 'Deposit your controversial food opinions here.',
-							placeholder:
-								'Example: peanut butter and mayonnaise sandwiches are delicious!',
-							initial_value: payload.text ?? '',
-							hint: 'What do you believe about food that people find appalling? Say it with your chest!',
-						}),
-						blocks.select({
-							id: 'spice_level',
-							label: 'How spicy is this opinion?',
-							placeholder: 'Select a spice level',
-							options: [
-								{ label: 'mild', value: 'mild' },
-								{ label: 'medium', value: 'medium' },
-								{ label: 'spicy', value: 'spicy' },
-								{ label: 'nuclear', value: 'nuclear' },
-							],
-						}),
 					],
 				}),
 			);
